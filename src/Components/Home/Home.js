c63bdf7bd38038865bd037bfc384ca63d0f60f12
vatepri <vatepri@yandex.ru>
2020-09-08 17:53:30 +0300

    add react-modal

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,6 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
+import ReactModal from "react-modal";
 import "./Home.css";
 
 export const Home = () => {
-  return <div className="home">Домашняя</div>;
+  const [isOpenModal, setIsOpenModal] = useState(false);
+  return (
+    <div className="home">
+      Домашняя
+      <button onClick={() => setIsOpenModal(!isOpenModal)}>Open Modal</button>
+      <ReactModal isOpen={isOpenModal} contentLabel="Minimal Modal Example">
+        <button onClick={() => setIsOpenModal(!isOpenModal)}>
+          Close Modal
+        </button>
+      </ReactModal>
+    </div>
+  );
 };
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { App } from './Components/App';
-
-ReactDOM.render(<App onLoad={() => fn()}/>, document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom";
+import { App } from "./Components/App";
+import Modal from "react-modal";
+Modal.setAppElement("#root");
+ReactDOM.render(<App onLoad={() => fn()} />, document.getElementById("root"));
